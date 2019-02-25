b473d667f04885033a978a68d6d37816c924dda9
Mathias <mathias.velilla@zentagroup.com>
2019-02-25 16:39:34 -0300

    Avanze curso MEAN UDEMY
    Rutas hijas

---
+++ adminpro/src/app/pages/pages.module.ts
@@ -0,0 +1,33 @@
+//MODULOS
+import { NgModule } from "@angular/core";
+import { SharedModule } from "../shared/shared.module";
+
+//RUTAS
+import { PAGES_ROUTES } from "./pages.routes";
+
+//COMPONENTES
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ProgressComponent } from "./progress/progress.component";
+import { Graficas1Component } from "./graficas1/graficas1.component";
+import { PagesComponent } from "./pages.component";
+
+
+
+@NgModule({
+    declarations:[
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+export class PagesModule {}
